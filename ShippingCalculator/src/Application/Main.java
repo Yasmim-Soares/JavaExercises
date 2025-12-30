fe6f12c08f9f3580744677c5424c51b7e9db2155
Yasmim <yasmimsousa251@gmail.com>
2025-12-30 12:03:42 -0300

    feat: main

--- ShippingCalculator/src/Application/Main.java
+++ ShippingCalculator/src/Application/Main.java
@@ -4,7 +4,40 @@ package Application;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        int[] totalFreights = new int[freights];
 
+        System.out.println("Enter the number of freights:");
+        int freights = sc.nextInt();
 
+        for(int i = 0; i < freights; i++){
+            System.out.print("Freight #" + (i+1) + " data:");
+            System.out.println("Standard or express (s/e)?");
+            String type = sc.nextLine();
+            
+            if(type.equals("s")){
+                System.out.print("Distance: ");
+                double distance = sc.nextDouble();
+                System.out.print("Weight:");
+                double weight = sc.nextDouble();
+
+                StandardFreight StandardFreight = new StandardFreight(distance, weight);
+            } else if(type.equals("e")) {
+                System.out.print("Distance: ");
+                double distance = sc.nextDouble();
+                System.out.print("Insurance Value: ");
+                double insuranceValue = sc.nextDouble;
+
+                ExpressFreight ExpressFreight = new ExpressFreight(distance, insuranceValue);
+            } else {
+                System.out.println("Invalid type. Please enter 's' or 'e'.")
+                i--;
+                continue;
+            }
+
+            System.out.println()
+            
+        }
     }
 }
